f7615b1d07a20bb7d9f57ec260bf95957f47332e
Dmitriy Good <goodamov@gmail.com>
2025-08-26 21:37:13 +0300

    Made something special

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        createBody();
+    }
+
+
+    public static void createBody() {
+        System.out.println("I'm alive! 0_0");
     }
 }
